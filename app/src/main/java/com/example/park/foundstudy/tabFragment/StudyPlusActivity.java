21a6f4afc3caa546d856a020c77ac20c62318b90
SJLee <susu_0128@naver.com>
2018-09-10 16:43:23 +0900

    StudyPlusActivity 추가 _ 최종본_ 180910

---
+++ app/src/main/java/com/example/park/foundstudy/tabFragment/StudyPlusActivity.java
@@ -0,0 +1,15 @@
+package com.example.park.foundstudy.tabFragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.park.foundstudy.R;
+
+public class StudyPlusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_study_plus);
+    }
+}
