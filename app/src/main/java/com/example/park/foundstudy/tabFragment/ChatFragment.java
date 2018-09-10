c052abbbb8b3f31e92d99523241f0575641b84ae
SJLee <susu_0128@naver.com>
2018-09-10 15:17:32 +0900

    Toast message Test 1 _ 180910

--- app/src/main/java/com/example/park/foundstudy/tabFragment/ChatFragment.java
+++ app/src/main/java/com/example/park/foundstudy/tabFragment/ChatFragment.java
@@ -24,7 +24,9 @@ public class ChatFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+        View view = inflater.inflate(R.layout.fragment_chat, container, false);
+
+        return view;
     }
 
 }
